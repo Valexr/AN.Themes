163413b93602a0bf5bcad2dfb248b5037f990405
Valexr <valexr@gmail.com>
2017-10-10 05:34:55 +0300

    36Edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -288,11 +288,53 @@ $('.artists-list').each(function() {
 
 
 // ISOTOPE ----------------------------------
-$('.grid').isotope({
-  itemSelector: '.grid-item',
+// $('.grid').isotope({
+//   itemSelector: '.grid-item',
+//   layoutMode: 'fitRows'
+//   // ...
+// });
+
+
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.news-snip',
   layoutMode: 'fitRows'
-  // ...
 });
+// filter items on button click
+$('.news-tags').on( 'click', 'a', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+// filter functions
+var filterFns = {
+  // show if number is greater than 50
+  numberGreaterThan50: function() {
+    var number = $(this).find('.number').text();
+    return parseInt( number, 10 ) > 50;
+  },
+  // show if name ends with -ium
+  ium: function() {
+    var name = $(this).find('.name').text();
+    return name.match( /ium$/ );
+  }
+};
+// bind filter button click
+$('.filters-button-group').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
+  $grid.isotope({ filter: filterValue });
+});
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+
+
 
 // WOW & Animate ----------------------------
 new WOW().init();
