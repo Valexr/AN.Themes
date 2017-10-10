1b3ccf7d6c2d1d7e4b285030a616edf7eb2d8858
Valexr <valexr@gmail.com>
2017-10-10 07:41:27 +0300

    41 - Re

--- assets/js/main.js
+++ assets/js/main.js
@@ -305,34 +305,31 @@ $('.news-tags').on( 'click', 'a', function() {
   var filterValue = $(this).attr('data-filter');
   $grid.isotope({ filter: filterValue });
 });
-// filter functions
-var filterFns = {
-  // show if number is greater than 50
-  numberGreaterThan50: function() {
-    var number = $(this).find('.number').text();
-    return parseInt( number, 10 ) > 50;
-  },
-  // show if name ends with -ium
-  ium: function() {
-    var name = $(this).find('.name').text();
-    return name.match( /ium$/ );
-  }
-};
-// bind filter button click
-$('.filters-button-group').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
-  // use filterFn if matches value
-  filterValue = filterFns[ filterValue ] || filterValue;
-  $grid.isotope({ filter: filterValue });
-});
+
 // change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
+$('.news-tags').each( function( i, tagsGroup ) {
+	var $tagsGroup = $( tagsGroup );
+	$tagsGroup.on( 'click', 'a', function() {
+		$tagsGroup.find('.active').removeClass('active');
+		$( this ).addClass('active');
+	});
 });
+
+// filter functions
+// var filterFns = {
+//   // show if number is greater than 50
+//   numberGreaterThan50: function() {
+//     var number = $(this).find('.number').text();
+//     return parseInt( number, 10 ) > 50;
+//   },
+//   // show if name ends with -ium
+//   ium: function() {
+//     var name = $(this).find('.name').text();
+//     return name.match( /ium$/ );
+//   }
+// };
+
+
 
 
 
@@ -378,9 +375,9 @@ if ($('body').is('.home-page')) {
 		},
 		exit: function(direction) {
 			$('.manifest-slogan').addClass('fixed')
-		$('.manifest-slogan').removeClass('relative')
-		$('.manifest-slogan').removeClass('absolute')
-		// $('.manifest').css( {'height': '220vh'} )
+			$('.manifest-slogan').removeClass('relative')
+			$('.manifest-slogan').removeClass('absolute')
+			// $('.manifest').css( {'height': '220vh'} )
 		},
 		exited: function(direction) {
 		// $('.manifest-slogan').addClass('absolute')
